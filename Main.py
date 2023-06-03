534631734e2866b23fa0865e41fe0884cff88106
thecorazon <xsaasa01@gmail.com>
2023-06-03 21:49:03 +0800

    5-ambil input data user

--- Main.py
+++ Main.py
@@ -1,63 +1,15 @@
-# Casting Tipe Data
-# merubah satu tipe ke tipe lain
-# tipe data {int, float, str, bool}
+# input User
 
+# # data dimasukkan adalah string
+# data = input("Masukkan data: ")
+# print("data = ", data, "type = ", type(data))
 
-## INTEGER
-print("+++++++Integer++++++")
-data_int = 9
-print("data = ", data_int, " tipe = ", type(data_int))
+# # Jikaingin mengambil integer maka kita casting ke integer
 
-data_float = float(data_int)
-data_str = str(data_int)
-data_bool = bool(data_int) # false hanya jika nilai int = 0
+# angka = float(input("Masukkan angka: "))
+# angka = int(input("Masukkan angka: "))
+# print("data = ", angka, "type = ", type(angka))
 
-
-print("data = ", data_float, " tipe = ", type(data_float))
-print("data = ", data_str, " tipe = ", type(data_str))
-print("data = ", data_bool, " tipe = ", type(data_bool))
-
-print('\n')
-
-## FLOAT
-print("+++++++Float++++++")
-data_float = 9.8
-print("data = ", data_float, " tipe = ", type(data_float))
-
-data_int = int(data_float)
-data_str = str(data_float)
-data_bool = bool(data_float) # false hanya jika nilai float = 0
-
-print("data = ", data_int, " tipe = ", type(data_int))
-print("data = ", data_str, " tipe = ", type(data_str))
-print("data = ", data_bool, " tipe = ", type(data_bool))
-
-print('\n')
-
-## bool
-print("+++++++bool++++++")
-data_bool = False
-print("data = ", data_bool, " tipe = ", type(data_bool))
-
-data_int = int(data_bool)
-data_str = str(data_bool)
-data_float = float(data_bool) # false hanya jika nilai float = 0
-
-print("data = ", data_int, " tipe = ", type(data_int))
-print("data = ", data_str, " tipe = ", type(data_str))
-print("data = ", data_float, " tipe = ", type(data_float))
-
-print('\n')
-
-## str
-print("+++++++str++++++")
-data_str = "";
-print("data = ", data_str, " tipe = ", type(data_str))
-
-# data_int = int(data_str) # string harus angka
-# data_float = float(data_str) # string harus angka
-data_bool = bool(data_str) # hanya bernilai false jika string kosong -> ""
-
-# print("data = ", data_int, " tipe = ", type(data_int))
-# print("data = ", data_float, " tipe = ", type(data_float))
-print("data = ", data_bool, " tipe = ", type(data_bool))
+# Untuk Boolen
+data_boolean = bool(int(input("masukkan nilai boolean: ")))
+print("data = ", data_boolean, "type = ", type(data_boolean))
