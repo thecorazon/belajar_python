914d958420addc21f6f3c255d1b1b9bf78a2f5c6
thecorazon <xsaasa01@gmail.com>
2023-06-03 21:02:32 +0800

    part 3

--- Main.py
+++ Main.py
@@ -1,32 +1,38 @@
-#variabel
+# tipe data
 
-# menyimpan / assigment
+# angka satuan (integer)
+data_integer = 100000
+print(type(data_integer))
+print("data : ", data_integer)
+print("- bertipe : ", type(data_integer))
 
+# float(desimal)
+data_float = 1.7
+print("data : ", data_float)
+print("- bertipe : ", type(data_float))
 
+# string(kumpulan karakter)
+data_string = "Kidomaru 10"
+print("data : ", data_string)
+print("- bertipe : ", type(data_string))
 
-a = 10
-x = 5
-panjang = 1000
-
-
-#pemanggilan pertama
-print("Nilai a = ",a)
-print("Nilai x = ",x)
-print("Nilai panjang = ",panjang)
-
-#penamaan
-nilai_y = 15 # tidak pake spasi
-juta10 = 100000 #angka tidak boleh di awal huruf
-
-nilaiZ = 17.5
-
-#pemanggilan ke 2
-print("Nilai x = ",x)
-x = 7
-print("Nilai x = ",x)
-b = x
-
-# assigment indirect
-print("Nilai b = ",b)
+# boolean (true/false)
+data_bool = False
+print("data : ", data_bool)
+print("- bertipe : ", type(data_bool))
 
 
+
+## tipe data khusus
+
+#bilangan kompleks
+data_complex = complex(5,6)
+print("data : ", data_complex)
+print("- bertipe : ", type(data_complex))
+
+# tipe data bahasa c
+
+from ctypes import c_double
+data_c_double = c_double(10.5)
+print("data : ", data_c_double)
+print("- bertipe : ", type(data_c_double))
